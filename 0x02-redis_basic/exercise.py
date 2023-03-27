64fecd208bf9db84a3c30a76e5345fe421306a0f
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-27 23:57:49 +0100

    feat: implement and add call_history decorator to Cache.store

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,21 @@ from typing import Union, Callable
 from functools import wraps
 
 
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(*args, **kwds):
+        in_key = "{}:inputs".format(method.__qualname__)
+        out_key = "{}:outputs".format(method.__qualname__)
+        inputs = str(args[1:])
+        outputs = method(*args, **kwds)
+
+        self = args[0]
+        self._redis.rpush(in_key, inputs)
+        self._redis.rpush(out_key, outputs)
+        return outputs
+    return wrapper
+
+
 def count_calls(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(*args, **kwds):
@@ -29,6 +44,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores data as a key value pair with uuid
         """
