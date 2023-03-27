41ae37aae936a844657700106754a878d424b1c4
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-27 21:46:48 +0100

    fix: downgrade type annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,19 +4,20 @@ Main file
 """
 import redis
 from uuid import uuid4
+from typing import Union
 
 
 class Cache:
-    """
+    """Cache class
     """
     def __init__(self):
-        """
+        """Initializes the Cache class
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: [str | bytes | int | float]) -> str:
-        """
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Stores data as a key value pair with uuid
         """
         key = str(uuid4())
         self._redis.set(key, data)
