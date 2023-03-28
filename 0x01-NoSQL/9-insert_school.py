8a9fa7a497c01659d5463c1dacdd4c1a19cadbf1
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-29 00:07:49 +0100

    feat: implement insert_school - inserts a new document in a collection using kwargs

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Contains the function insert_school
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ Inserts a document in a collection using kwargs
+    """
+    res = mongo_collection.insertOne(kwargs)
+    return res.inserted_id
