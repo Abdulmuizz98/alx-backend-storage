c4d43b9974c570d4ea7c53fc0ae02ad2888d975f
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-04-02 10:16:28 +0100

    feat: create AddBonus procedure

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,14 @@
+-- Stored procedure AddBonus that adds a new correction for a student.
+DELIMITER //
+CREATE PROCEDURE IF NOT EXISTS AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+DECLARE pid INT DEFAULT 0;
+SELECT id INTO pid FROM projects WHERE projects.name = project_name;
+IF pid = 0
+THEN
+INSERT INTO projects (name) VALUES (project_name);
+SET pid = LAST_INSERT_ID();
+END IF;
+INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, pid, score);
+END
+//
